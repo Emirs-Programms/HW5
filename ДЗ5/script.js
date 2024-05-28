ed1c8be3f444d6b22fbbf6b66a96f136b2b318c3
Emirs-Programms <emirmarash@gmail.com>
2024-05-28 19:06:12 +0600

    Add files via upload

---
+++ ДЗ5/script.js
@@ -0,0 +1,19 @@
+function output() {
+    const inputText = document.getElementById("Input").value;
+    const text = document.getElementById("h1");
+    text.textContent = inputText;
+}
+
+
+
+let count = 0;
+    const counterElement = document.getElementById('counter');
+
+    function updateCounter() {
+        counterElement.textContent = count;
+    }
+
+
+
+
+    
